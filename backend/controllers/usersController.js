bb48905dff9d545e58852764bc011fe83165f545
abdul rauf <roehan-abdul-rauf@laplateforme.io>
2023-01-09 17:28:23 +0100

    Modification des controllers User et Message
    - Ajout de foonctionnalité inexistent (creation et modification de function)

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -6,46 +6,44 @@ const User = require('../models/userModel');
 // @desc Recupérer tous les users
 // @route GET /api/users
 // @access private
-const getAllUsers = asyncHandler(async (req,res) => {
-
-    res.status(200).json({response:"get all users"});
+const getAllUsers = asyncHandler(async (req, res) => {
+    User.find()
+        .then((user) => {
+            res.status(200).json(user);
+        })
+        .catch((error) => res.status(400).json({ error: "Aucun Utilisateurs" }));
 })
 
 
 // @desc Recupérer un user
 // @route GET /api/users/:id
 // @access private
-const getUser = asyncHandler(async (req,res) => {
-
-    res.status(200).json({response:`get user by id ${req.params.id}`})
+const getUser = asyncHandler(async (req, res) => {
+    await User.findById(req.params.id)
+        .then((user) => {
+            res.status(200).json(user);
+        })
+        .catch((error) => res.status(400).json({ error: "l'Utilisateur n'existe pas" }));
 })
 
-
-// @desc Modifier infos user
-// @route PUT /api/users/:id
-// @access private
-const updateUser = asyncHandler(async (req,res) => {
-    
-    res.status(200).json({response:`modifier infos user ${req.params.id}`})
-})
 
 
 // @desc inscription d'un utilisateur
-// @route POST /api/users
+// @route POST /api/users/
 // @access public
-const registerUser = asyncHandler(async (req,res) => {
+const registerUser = asyncHandler(async (req, res) => {
 
-    const { pseudo,mail,password } = req.body;
+    const { pseudo, mail, password } = req.body;
 
-    if(!pseudo || !mail || !password){
+    if (!pseudo || !mail || !password) {
 
         res.status(400);
         throw new Error('Please add all fields');
     }
 
     // Utilisateur déjà éxistant
-    const userExists = await User.findOne({mail});
-    if(userExists){
+    const userExists = await User.findOne({ mail });
+    if (userExists) {
         res.status(400);
         throw new Error('Adresse mail déja utilisée');
     }
@@ -58,17 +56,18 @@ const registerUser = asyncHandler(async (req,res) => {
     const user = await User.create({
         pseudo,
         mail,
-        password:hashedPassword
+        password: hashedPassword
     })
 
-    if(user){
+    if (user) {
         res.status(200);
         res.json({
-            _id:user.id,
-            pseudo:user.pseudo,
-            email:user.mail
+            _id: user.id,
+            pseudo: user.pseudo,
+            email: user.mail,
+            token: generateToken(user._id),
         })
-    }else{
+    } else {
         res.status(400);
         throw new Error('Données utilisateur non valides')
     }
@@ -79,9 +78,129 @@ const registerUser = asyncHandler(async (req,res) => {
 // @desc Authentifier un utilisateur
 // @route POST /api/users/login
 // @access public
-const login = asyncHandler( async (req,res) => {
-    
-    res.status(201).json({response:"register"});
+const login = asyncHandler(async (req, res) => {
+    const { mail, password } = req.body;
+
+    // Vérifier si l'utilisateur existe
+    const user = await User.findOne({ mail });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            pseudo: user.pseudo,
+            email: user.mail,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(401);
+        throw new Error('Identifiants ou mdp incorrects');
+    }
+})
+
+// Génération du token
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
+
+// @desc Récupérer les données d'user
+// @route GET /api/users/me
+// @access private
+
+const getMe = asyncHandler(async (req, res) => {
+    const { id, mail, pseudo } = await User.findById(req.user.id);
+
+    res.status(200).json({
+        id,
+        email: mail,
+        pseudo,
+    })
+})
+
+
+// @desc Modifier infos user
+// @route PUT /api/users/:id
+// @access private
+const updateUser = asyncHandler(async (req, res) => {
+
+    const { pseudo, mail, password } = req.body;
+
+    // Vérifier si au moins un champs est remplir
+    if (pseudo || mail || password) {
+
+        const user = await User.findById(req.params.id);
+
+        // Vérifier si l'utilisateur existe
+        if (!req.params.id) {
+            res.status(400);
+            throw new Error("Aucun utilisateur trouvé");
+        } else {
+            // Vérifier si le champs pseudo est remplir
+            if (pseudo) {
+                // Vérifier si l'utilisateur existe puis le modifier
+                User.findOneAndUpdate(
+                    { _id: req.params.id },
+                    {
+                        $set: {
+                            pseudo: pseudo,
+                        },
+                    }).then(() => {
+                        res.status(200).json({
+                            message: "Utilisateur bien modifiée !"
+                        })
+                    })
+            }
+
+            // Vérifier si le champs mail est remplir
+            if (mail) {
+                // Vérifier si l'utilisateur existe puis le modifier
+                User.findOneAndUpdate(
+                    { _id: req.params.id },
+                    {
+                        $set: {
+                            mail: mail,
+                        },
+                    }).then(() => {
+                        res.status(200).json({
+                            message: "Utilisateur bien modifiée !"
+                        })
+                    })
+            }
+
+            // Vérifier si le champs password est remplir
+            if (password) {
+                // Vérifier si l'utilisateur existe puis le modifier
+                User.findOneAndUpdate(
+                    { _id: req.params.id },
+                    {
+                        $set: {
+                            password: password,
+                        },
+                    }).then(() => {
+                        res.status(200).json({
+                            message: "Utilisateur bien modifiée !"
+                        })
+                    })
+            }
+        }
+    } // Vérifier si aucun champs est remplir
+    else if (!pseudo || !mail || !password) {
+        res.status(400);
+        throw new Error('Veuillez remplir au moins un champs !');
+    }
+})
+
+
+// @desc Supprimer un user
+// @route DELETE /api/users/:id
+// @access private
+const deleteUser = asyncHandler(async (req, res) => {
+    User.deleteOne({ _id: req.params.id })
+        .then(() => res.json({ message: "l'utilisateur à bien été supprimer" }))
+        .catch((error) => res.status(400).json({ error: "Aucun Utilisateur trouvé" }));
+
 })
 
 
@@ -89,5 +208,8 @@ module.exports = {
     getAllUsers,
     getUser,
     registerUser,
-    updateUser
+    login,
+    getMe,
+    updateUser,
+    deleteUser,
 };
