d51394cf8b4196a375c64d18e61c5aa36e94dc49
abdul rauf <roehan-abdul-rauf@laplateforme.io>
2023-01-09 17:30:39 +0100

    Modification de routes Users et Messages
    - Ajout de routes manquant et de middleware

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const { getAllUsers, getUser, registerUser, updateUser } = require('../controllers/usersController');
+const { getAllUsers, getUser, registerUser, login, getMe, updateUser, deleteUser } = require('../controllers/usersController');
 const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
 
-router.route('/:id').get(getUser).put(updateUser);
-router.get('/',getAllUsers);
-
-
-router.post('/', registerUser);
-
-
-
+router.route('/').post(registerUser).get(protect, getAllUsers);
+router.route('/:id').get(protect, getUser).delete(protect, deleteUser).put(protect, updateUser);
+router.post('/login', login);
+router.get('/me', protect, getMe);
 
 module.exports = router;
