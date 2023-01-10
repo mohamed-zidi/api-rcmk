01c0cd2c7b02cc505c3c38306b3236531cf6f34b
abdul rauf <roehan-abdul-rauf@laplateforme.io>
2023-01-10 10:55:40 +0100

    Creation de routes pour admin
    - Ajout de routes ../admin/:id pour users et messages

--- backend/routes/messagesRoute.js
+++ backend/routes/messagesRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getAllMessages, setMessage, deleteMessage, getMessage,updateMessage } = require('../controllers/messagesController');
+const { getAllMessages, setMessage, deleteMessage, getMessage,updateMessage, deleteMessageAdmin } = require('../controllers/messagesController');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getAllMessages).post(protect, setMessage);
 router.route('/:id').get(protect, getMessage).delete(protect, deleteMessage).put(protect, updateMessage);
+router.route('/admin/:id').delete(protect, deleteMessageAdmin);
 
 module.exports = router;
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllUsers, getUser, registerUser, login, getMe, updateUser, deleteUser } = require('../controllers/usersController');
+const { getAllUsers, getUser, registerUser, login, getMe, updateUser, deleteUser, updateUserAdmin, deleteUserAdmin } = require('../controllers/usersController');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').post(registerUser).get(protect, getAllUsers);
 router.route('/:id').get(protect, getUser).delete(protect, deleteUser).put(protect, updateUser);
+router.route('/admin/:id').delete(protect, deleteUserAdmin).put(protect, updateUserAdmin);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 
